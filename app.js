4a33c61551e6247a97b3f987048825ec7aa037c2
SiddhantPawar03 <siddhantpawar1203@gmail.com>
2023-03-23 23:39:48 +0530

    Version-2

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const ejs = require('ejs');
 const _ = require('lodash');
 const mongoose = require('mongoose');
 
-
 const app = express();
 
 app.set('views', './views');
@@ -20,6 +19,7 @@ const crimeSchema = {
     id: Number,
     date: String,
     day: String,
+    time: String,
     district: String,
     address: String,
     xcoord: Number,
@@ -29,11 +29,8 @@ const crimeSchema = {
 const Crime = mongoose.model("Post", crimeSchema);
 
 
-
-
-
 app.get("/", function(req,res) {
-    res.send("HELLO!");
+    res.render("home");
 });
 
 app.get("/dashboard", function(req,res) {
@@ -45,42 +42,41 @@ app.get("/complaint", function(req,res) {
 });
 
 app.post("/complaint", function(req,res) {
+  function getTime() {
+    var date = new Date();
+    var currentTime = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    return currentTime;
+  }
     const complaint = new Crime({
-        // date: req.body.date,
+        date: req.body.date,
         day: req.body.days,
+        time: getTime(),
         district: req.body.district,
         address: req.body.address,
         xcoord: req.body.xcoord,
         ycoord: req.body.ycoord
     });
-    console.log(req.body.address);
 
     complaint.save(function(err) {
         if(!err) {
-            res.redirect("/");
+            res.redirect("/complaint");
         }
     });
 });
+
+
 
 
 app.get("/viewcrime", async (req, res) => {
     try {
       const details = await Crime.find({ });
     //   res.send(articles);
-      res.render("viewcrime", {listTitle: "Crime Report", item: details});
-      console.log(details.day);
+      res.render("viewcrime", {listTitle: "Crime Report", 'item': details});
+      console.log(details);
     } catch (err) {
       console.log(err);
     }
   });
-
-//   app.get("/viewcrime", function(req,res) {
-//     Crime.find({}, function(err, details){
-//         res.render("viewcrime", {
-//             listTitle: "Crime Report", details: details
-//           });
-//       });
-//   });
 
   app.get("/", function(req, res){
 
