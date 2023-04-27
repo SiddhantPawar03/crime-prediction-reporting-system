80f808d754b9e4684f2aaa96d5126112cc7d7009
SiddhantPawar03 <siddhantpawar1203@gmail.com>
2023-04-27 14:24:45 +0530

    add:cloudDB

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { forEach } = require('lodash');
 // const userRouter = require('./views/routes/userRoutes');
 
 const app = express();
-
+require('dotenv').config();
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
@@ -16,7 +16,9 @@ app.use(express.static("public"));
 app.use(express.json());
 // app.use("/users", userRouter);
 
-mongoose.connect("mongodb://localhost:27017/crimeDB", {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true})
+.then(data => console.log("Database connected"))
+.catch(err => console.log("Database connection failed"))
 
 
 const crimeSchema = {
